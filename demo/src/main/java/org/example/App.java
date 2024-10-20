ac88fb108e5974a8ecccf2572ce6994d9e50629b
Raveendra Hennyake <raveendralakshan5@gmail.com>
2024-10-20 18:41:08 +0530

    bubbleSortinImplement

--- demo/src/main/java/org/example/App.java
+++ demo/src/main/java/org/example/App.java
@@ -1,13 +1,16 @@
 package org.example;
+public class App {
+    public static void main(String[] args) {
+        int [] arr={64, 34, 25, 12, 22, 11, 90};
+        BubbleSort arrs=new BubbleSort();
+        arrs.bubbleSort(arr);
+        arrs.printArray(arr);
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+
+
     }
+
+
 }
+
---
+++ demo/src/main/java/org/example/BubbleSort.java
@@ -0,0 +1,29 @@
+package org.example;
+public class BubbleSort {
+    public void bubbleSort(int[] arr) {
+        int length = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < length - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    public void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
